b4ce1c9100d7dff7bc853fb3bb01da2723744e04
Pablo Querales <pabloquerales20@gmail.com>
2023-05-31 08:01:38 +0200

    inicio de reto 30DaysOfJS - DIA 1

---
+++ Day1/introduction.js
@@ -0,0 +1,31 @@
+console.log('Bienvenido a 30DaysOfJavaScript')
+
+// Declarando diferentes variables de diferentes tipos de datos
+let nombre = 'Asabeneh' // nombre de una persona
+let apellido = 'Yetayeh' // apellido de una persona
+let pais = 'Finland' // país
+let ciudad = 'Helsinki' // ciudad capital
+let edad = 100 // edad en años
+let estaCasado = true
+
+console.log(nombre, apellido, pais, ciudad, edad, estaCasado)
+
+// Declarando variables con valores numéricos
+const gravedad = 9.81 // gravedad terrestre en m/s2
+const puntoDeEbullicion  = 100 // punto de ebullición del agua, temperatura en oC
+const PI = 3.14 // constante geométrica
+
+console.log( gravedad, puntoDeEbullicion, PI)
+
+
+// Las variables también pueden ser declaradas en una línea separada por una coma
+trabajo = 'profesor',
+viveEn = 'Finlandia';
+console.log(nombre, trabajo, viveEn);
+
+// "los comentarios pueden hacer que el código sea legible"
+
+// "Bienvenido a 30DaysOfJavaScript"
+
+// "Los comentarios pueden hacer el código legible,
+// fácil de usar e informativo"
---
+++ Day1/variables.js
@@ -0,0 +1,11 @@
+const apodo = "Klishe"
+const numero = 100
+const real = true
+const nulo = null
+const indefinido = undefined
+
+
+
+
+
+console.log(apodo , numero , real , nulo , indefinido )
